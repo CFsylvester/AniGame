b3f61aeb74aee831f63c1fb8bd716b0c29ee991e
Tom Chestnut <tchestnut85@gmail.com>
2020-09-19 13:30:44 -0400

    updated anime fetch requests and made a for loop to change star icons based on animeRating.

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,52 +32,115 @@ function gameRequest(gameName) {
             // fetch gameName from kitsu api
             return fetch("https://kitsu.io/api/edge/anime?filter[text]=" + gameName);
         })
-        .then(function (kitsuResponse) {
-            return kitsuResponse.json();
+        .then(function (animeResponse) {
+            return animeResponse.json();
         })
-        .then(function (kitsuResponse) {
-            console.log(kitsuResponse);
-            // add endpoints here from the Kitsu API related to gameName value and insert into HTML elements
+        .then(function (animeResponse) {
+            console.log(animeResponse);
+            // Kitsu API results related to gameName value
 
             // create a container for all the kitsu api results
-            var kitsuContainer = document.createElement("div");
-            kitsuContainer.setAttribute("id", "anime-container");
-            kitsuContainer.classList = "columns is-vcentered anime-class has-background-grey-dark";
-            resultsContainer.appendChild(kitsuContainer);
+            var animeContainer = document.createElement("div");
+            animeContainer.setAttribute("id", "anime-container");
+            animeContainer.classList = "columns is-vcentered anime-class has-background-grey-lighter";
+            resultsContainer.appendChild(animeContainer);
 
             // div to contain title, rating and description
-            var kitsuInfoEl = document.createElement("div");
-            kitsuInfoEl.setAttribute("id", "anime-info");
-            kitsuInfoEl.classList = "column container is-family-monospace is-size-5 has-text-light";
-            kitsuContainer.appendChild(kitsuInfoEl);
+            var animeInfoEl = document.createElement("div");
+            animeInfoEl.setAttribute("id", "anime-info");
+            animeInfoEl.classList = "column container is-family-monospace is-size-5 has-text-black-bis";
+            animeContainer.appendChild(animeInfoEl);
 
-            // display title rating and description
-            kitsuInfoEl.innerHTML = kitsuResponse.data[0].attributes.canonicalTitle
-                + "<br>Rating: <span id='anime-rating' class='far fa-star'>" + kitsuResponse.data[0].attributes.averageRating + "</span><p id='anime-p' class='is-size-6'>" + kitsuResponse.data[0].attributes.description + "</p>";
+            // display title 
+            var animeTitle = document.createElement("h2");
+            animeTitle.setAttribute("id", "anime-title");
+            animeTitle.classList = "column container has-text-centered is-family-monospace is-size-3 has-text-black-bis";
+            animeTitle.innerHTML = animeResponse.data[0].attributes.canonicalTitle;
+            animeInfoEl.appendChild(animeTitle);
+
+            // display rating/stars
+            var animeRatingEl = document.createElement("span");
+            animeRatingEl.setAttribute("id", "anime-rating");
+            var animeStar1 = document.createElement("i");
+            animeStar1.classList = "far fa-star";
+            var animeStar2 = document.createElement("i");
+            animeStar2.classList = "far fa-star";
+            var animeStar3 = document.createElement("i");
+            animeStar3.classList = "far fa-star";
+            var animeStar4 = document.createElement("i");
+            animeStar4.classList = "far fa-star";
+            var animeStar5 = document.createElement("i");
+            animeStar5.classList = "far fa-star";
+
+            animeRatingEl.classList = "column container has-text-centered is-family-monospace is-size-4 has-text-black-bis";
+            animeInfoEl.appendChild(animeRatingEl);
+            animeRatingEl.appendChild(animeStar1);
+            animeRatingEl.appendChild(animeStar2);
+            animeRatingEl.appendChild(animeStar3);
+            animeRatingEl.appendChild(animeStar4);
+            animeRatingEl.appendChild(animeStar5);
+
+            // create an if else statement to highlight or fill star icons depending on value of animeRating
+            var animeRating = animeResponse.data[0].attributes.averageRating;
+            console.log(animeRating);
+
+            if (animeRating < 30) {
+                animeStar1.classList.remove("far");
+                animeStar1.classList.add("fas");
+            } else if (animeRating > 30 && animeRating < 50) {
+                animeStar1.classList.remove("far");
+                animeStar1.classList.add("fas");
+                animeStar2.classList.remove("far");
+                animeStar2.classList.add("fas");
+            } else if (animeRating > 50 && animeRating < 70) {
+                animeStar1.classList.remove("far");
+                animeStar1.classList.add("fas");
+                animeStar2.classList.remove("far");
+                animeStar2.classList.add("fas");
+                animeStar3.classList.remove("far");
+                animeStar3.classList.add("fas");
+            } else if (animeRating > 70 && animeRating < 90) {
+                animeStar1.classList.remove("far");
+                animeStar1.classList.add("fas");
+                animeStar2.classList.remove("far");
+                animeStar2.classList.add("fas");
+                animeStar3.classList.remove("far");
+                animeStar3.classList.add("fas");
+                animeStar4.classList.remove("far");
+                animeStar4.classList.add("fas");
+            } else if (animeRating > 90 && animeRating <= 100) {
+                animeStar1.classList.remove("far");
+                animeStar1.classList.add("fas");
+                animeStar2.classList.remove("far");
+                animeStar2.classList.add("fas");
+                animeStar3.classList.remove("far");
+                animeStar3.classList.add("fas");
+                animeStar4.classList.remove("far");
+                animeStar4.classList.add("fas");
+                animeStar5.classList.remove("far");
+                animeStar5.classList.add("fas");
+            };
+
+            // display description
+            var animeDescription = document.createElement("p");
+            animeDescription.setAttribute("id", "anime-description");
+            animeDescription.classList = "column container has-text-left is-family-monospace is-size-6 has-text-black-bis";
+            animeDescription.innerHTML = animeResponse.data[0].attributes.description;
+            animeInfoEl.appendChild(animeDescription);
 
             // display video of the anime
             var animeVidContainer = document.createElement("div");
             animeVidContainer.classList = "is-centered";
-            kitsuContainer.appendChild(animeVidContainer);
+            animeContainer.appendChild(animeVidContainer);
 
             var animeVid = document.createElement("iframe");
             animeVid.setAttribute("id", "anime-video");
             animeVid.classList = "column is-centered";
-            animeVid.setAttribute("src", "https://www.youtube.com/embed/" + kitsuResponse.data[0].attributes.youtubeVideoId + "?controls=1");
+            animeVid.setAttribute("src", "https://www.youtube.com/embed/" + animeResponse.data[0].attributes.youtubeVideoId + "?controls=1");
             animeVid.setAttribute("width", "560px");
             animeVid.setAttribute("height", "340px");
             animeVid.setAttribute("alt", "Trailer for " + gameName);
             animeVidContainer.appendChild(animeVid);
-
-            // display main image of the anime
-            // var animeImg = document.createElement("img");
-            // animeImg.classList = "column";
-            // animeImg.setAttribute("id", "anime-image");
-            // animeImg.setAttribute("src", kitsuResponse.data[0].attributes.coverImage.tiny);
-            // animeImg.setAttribute("width", kitsuResponse.data[0].attributes.coverImage.meta.dimensions.tiny.width);
-            // animeImg.setAttribute("height", kitsuResponse.data[0].attributes.coverImage.meta.dimensions.tiny.height);
-            // animeImg.setAttribute("alt", "Image of " + gameName);
-            // kitsuContainer.appendChild(animeImg);
 
         })
         .catch(function (error) {
