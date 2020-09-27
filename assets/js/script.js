506483378b69b81c21258872ef2da325de491dde
Claire Sylvester <clairesylvstr@gmail.com>
2020-09-27 00:48:24 -0400

    API swtich

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,24 +14,24 @@ var nintendoEl = document.querySelector("#nintendo");
 var steamEl = document.querySelector("#steam");
 
 function gameRequest(gameName) {
-    var gameApi = "https://cors-anywhere.herokuapp.com/http://www.gamespot.com/api/games/?api_key=" + gamespotKey + "&filter=name:" + gameName + "&format=json&limit=6";
+    var gameApi = "https://api.rawg.io/api/games?page_size=5&search=" + gameName;
     // fetch the response
     fetch(gameApi)
         .then(function (response) {
             return response.json();
         })
         .then(function (response) {
-            // console.log(response);
+            console.log(response);
             gameResultsEl.innerHTML = "Game(s) found for " + gameName + ":";
             //Clear gameColumnsContainerEl
             $(gameColumnsContainerEl).empty();
             for (var i = 0; i < response.results.length; i++) {
                 var gameColumnEl = document.createElement("div");
                 gameColumnEl.setAttribute("id", "game-column");
-                gameColumnEl.setAttribute('data-description', response.results[i].description);
+                //gameColumnEl.setAttribute('data-description', response.results[i].name);
+                gameColumnEl.setAttribute("data-name", response.results[i].name); 
                 gameColumnEl.setAttribute("class", "column has-background-grey-light search-results is-two-fifths has-text-black-bis mx-3 my-4");
                 gameColumnsContainerEl.appendChild(gameColumnEl);
-
                 //Columns 1 (baseSearchEl) Stores IMAGE and TITLE
                 var baseSearchEl = document.createElement("div");
                 baseSearchEl.setAttribute("id", "base-search");
@@ -42,7 +42,6 @@ function gameRequest(gameName) {
                 baseClickEl.setAttribute("id", "base-click");
                 baseClickEl.setAttribute("class", "columns is-mobile");
                 gameColumnEl.appendChild(baseClickEl);
-                //$(baseClickEl).hide();
                 //Columns 1 (baseSearchEl) Column 1 (columnImageEl) Stores Image Column 
                 var columnImageEl = document.createElement("div");
                 columnImageEl.setAttribute("id", "column-image");
@@ -51,8 +50,9 @@ function gameRequest(gameName) {
                 //Image
                 var gameImg = document.createElement("img");
                 gameImg.setAttribute("id", "game-image");
-                gameImg.setAttribute("src", response.results[i].image.square_tiny);
-                gameImg.setAttribute("alt", "Image of " + gameName);
+                gameImg.setAttribute("src", response.results[i].background_image);
+                $(gameImg).addClass("image");
+                gameImg.setAttribute("alt", "Image of " + response.results[i].name);
                 columnImageEl.appendChild(gameImg);
                 //Columns 1 (baseSearchEl) Column 2 (gameHeaderEl) Stores Title and Rating
                 var gameHeaderEl = document.createElement("div");
@@ -65,48 +65,97 @@ function gameRequest(gameName) {
                 gameHeaderEl.appendChild(gameTitleEl);
                 var gameTitleText = document.createTextNode(response.results[i].name);
                 gameTitleEl.appendChild(gameTitleText);
-
                 //Create Columns 2 (baseClickEl) Column 1 For Description (columnDescriptionEl)
                 var columnDescriptionEl = document.createElement("div");
                 columnDescriptionEl.setAttribute("id", "column-description");
                 columnDescriptionEl.setAttribute("class", "column");
                 baseClickEl.appendChild(columnDescriptionEl);
                 //Game Stars Element
-                // var gameStarsEl = document.createElement("div");
-                // gameStarsEl.setAttribute("id", "game-stars");
-                // gameStarsEl.setAttribute("class", "container has-text-centered");
-                // //columnDescriptionEl.appendChild(gameStarsEl);
-                // //Game Stars
-                // var gameTitleStarSpan = document.createElement("h3");
-                // gameTitleStarSpan.setAttribute("class", "title has-text-centered is-size-3");
-                // gameStarsEl.appendChild(gameTitleStarSpan);
-                // var gameTitleStar1 = document.createElement("i");
-                // var gameTitleStar2 = document.createElement("i");
-                // var gameTitleStar3 = document.createElement("i");
-                // var gameTitleStar4 = document.createElement("i");
-                // var gameTitleStar5 = document.createElement("i");
-                // gameTitleStar1.setAttribute("class", "fas fa-star");
-                // gameTitleStar2.setAttribute("class", "fas fa-star");
-                // gameTitleStar3.setAttribute("class", "fas fa-star");
-                // gameTitleStar4.setAttribute("class", "fas fa-star");
-                // gameTitleStar5.setAttribute("class", "fas fa-star");
-                // gameTitleStarSpan.appendChild(gameTitleStar1);
-                // gameTitleStarSpan.appendChild(gameTitleStar2);
-                // gameTitleStarSpan.appendChild(gameTitleStar3);
-                // gameTitleStarSpan.appendChild(gameTitleStar4);
-                // gameTitleStarSpan.appendChild(gameTitleStar5);
+                var gameStarsEl = document.createElement("div");
+                gameStarsEl.setAttribute("id", "game-stars");
+                gameStarsEl.setAttribute("class", "container has-text-centered");
+                gameHeaderEl.appendChild(gameStarsEl);
+                //Game Stars
+                var gameTitleStarSpan = document.createElement("h3");
+                gameTitleStarSpan.setAttribute("class", "title has-text-centered is-size-3");
+                gameStarsEl.appendChild(gameTitleStarSpan);
+                var gameTitleStar1 = document.createElement("i");
+                var gameTitleStar2 = document.createElement("i");
+                var gameTitleStar3 = document.createElement("i");
+                var gameTitleStar4 = document.createElement("i");
+                var gameTitleStar5 = document.createElement("i");
+                gameTitleStar1.setAttribute("class", "far fa-star");
+                gameTitleStar2.setAttribute("class", "far fa-star");
+                gameTitleStar3.setAttribute("class", "far fa-star");
+                gameTitleStar4.setAttribute("class", "far fa-star");
+                gameTitleStar5.setAttribute("class", "far fa-star");
+                gameTitleStarSpan.appendChild(gameTitleStar1);
+                gameTitleStarSpan.appendChild(gameTitleStar2);
+                gameTitleStarSpan.appendChild(gameTitleStar3);
+                gameTitleStarSpan.appendChild(gameTitleStar4);
+                gameTitleStarSpan.appendChild(gameTitleStar5);
+                //Game Rating if/else
+                var gameRating = response.results[i].rating_top; 
+                console.log(gameRating); 
+                if (gameRating <=1) {
+                    gameTitleStar1.classList.remove("far");
+                    gameTitleStar1.classList.add("fas");
+                } else if (gameRating <= 2 ) {
+                    gameTitleStar1.classList.remove("far");
+                    gameTitleStar1.classList.add("fas");
+                    gameTitleStar2.classList.remove("far");
+                    gameTitleStar2.classList.add("fas");
+                } else if (gameRating <= 3) {
+                    gameTitleStar1.classList.remove("far");
+                    gameTitleStar1.classList.add("fas");
+                    gameTitleStar2.classList.remove("far");
+                    gameTitleStar2.classList.add("fas");
+                    gameTitleStar3.classList.remove("far");
+                    gameTitleStar3.classList.add("fas");
+                } else if (gameRating <= 4) {
+                    gameTitleStar1.classList.remove("far");
+                    gameTitleStar1.classList.add("fas");
+                    gameTitleStar2.classList.remove("far");
+                    gameTitleStar2.classList.add("fas");
+                    gameTitleStar3.classList.remove("far");
+                    gameTitleStar3.classList.add("fas");
+                    gameTitleStar4.classList.remove("far");
+                    gameTitleStar4.classList.add("fas");
+                } else if (gameRating <= 5) {
+                    gameTitleStar1.classList.remove("far");
+                    gameTitleStar1.classList.add("fas");
+                    gameTitleStar2.classList.remove("far");
+                    gameTitleStar2.classList.add("fas");
+                    gameTitleStar3.classList.remove("far");
+                    gameTitleStar3.classList.add("fas");
+                    gameTitleStar4.classList.remove("far");
+                    gameTitleStar4.classList.add("fas");
+                    gameTitleStar5.classList.remove("far");
+                    gameTitleStar5.classList.add("fas");
+                };
                 //Summary Text
                 var gameSummary = document.createElement("p");
                 columnDescriptionEl.appendChild(gameSummary);
-                $(gameSummary).text(response.results[i].description);
                 $(gameSummary).hide();
-
-
+                //Game Description Toggle + Fetch
                 $(".search-results").click(function () {
                     var gameSummaryClick = ($(this).find("p"));
-                    $(gameSummaryClick).toggle(); 
                     $(this).removeClass("is-two-fifths");
                     $(this).addClass("is-four-fifths");
+                    var titleName = ($(this).attr("data-name"))
+                    var shortTitleName = titleName.split(",", 1);
+                    var gameDescriptionApi = "https://cors-anywhere.herokuapp.com/http://www.gamespot.com/api/games/?api_key=" + gamespotKey + "&filter=name:" + shortTitleName + "&format=json&limit=6";
+                    fetch(gameDescriptionApi)
+                        .then(function (ratingResponse) {
+                            return ratingResponse.json();
+                        })
+                        .then(function (ratingResponse) {
+                            console.log(ratingResponse);
+                            $(gameSummaryClick).text(ratingResponse.results[0].description); 
+                            $(gameSummaryClick).toggle();
+                        })
+                        .catch(function (error) {
+                        });
                 });
             }
             //Anime Fetch Call
@@ -161,6 +210,8 @@ function animeRequest(gameName) {
                 // create a container for all the kitsu api results
                 var animeContainer = document.createElement("div");
                 animeContainer.setAttribute("id", "anime-container");
+                animeContainer.setAttribute("data-animeCanon", animeResponse.data[i].attributes.canonicalTitle);
+                console.log(animeResponse); 
                 animeContainer.classList = "column anime-class has-background-grey-light has-text-black-bis search-results-anime is-two-fifths has-text-centered mx-3 my-4";
                 resultsContainerEL.appendChild(animeContainer);
                 // div to contain title, rating, and description
@@ -263,11 +314,23 @@ function animeRequest(gameName) {
                 $(this).addClass("is-four-fifths");
                 var animeContainerClick = ($(this).find("p"));
                 var animeFindSpan = ($(this).find("span"));
-                $(animeContainerClick).toggle(); 
-                $(animeFindSpan).toggle(); 
+                $(animeContainerClick).toggle();
+                $(animeFindSpan).toggle();
+                var animeCanon = $(this).attr("data-animeCanon");
+                fetch("https://kitsu.io/api/edge/anime?filter[text]=" + animeCanon)
+                    .then(function (streamResponse) {
+                        return streamResponse.json();
+                    })
+                    .then(function (streamResponse) {
+                        console.log(streamResponse.data[i].relationships.streamingLinks.links.related)
+                    })
+                    .catch(function (error) {
+                    });
             });
-
+            // return fetch("https://kitsu.io/api/edge/streaming-links?filter[id]=" + animeResponse.data[i].id);
+            // return fetch("https://kitsu.io/api/edge/streaming-links?url=" + gameName);
         });
+
 };
 
 // var searchGame = function (event) {
